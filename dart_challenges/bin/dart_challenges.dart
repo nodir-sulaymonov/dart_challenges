eae517440c323a4057547ad38963ef6d8616c915
nadir <s.nodir9494@gmail.com>
2020-09-19 18:56:44 +0300

    crate project dart

---
+++ dart_challenges/bin/dart_challenges.dart
@@ -0,0 +1,5 @@
+import 'package:dart_challenges/dart_challenges.dart' as dart_challenges;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_challenges.calculate()}!');
+}
---
+++ dart_challenges/test/dart_challenges_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_challenges/dart_challenges.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
